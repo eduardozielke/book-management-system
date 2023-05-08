699ee4ebeaca22ddf7b43f76bfa69a71edc4bd4e
Eduardo Zielke <zielkeeduardo@gmail.com>
2023-05-08 17:50:43 -0300

    first commit

---
+++ src/test/java/com/eduardo/zielke/bookmanagementsystem/BookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.zielke.bookmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
